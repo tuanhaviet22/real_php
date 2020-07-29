b888a48f496036b508cfdca4c5097cbd4629311f
tuanha <tuanhaviet22@gmail.com>
2020-07-30 00:15:53 +0700

    init project

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'real_php');
+
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if($link === false){
+    die('Không thể kết nối với cơ sở dữ liệu');
+}
